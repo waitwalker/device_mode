f2ea95f9623b644d54d69f6cd2696a7da2266220
waitwalker <waitwalker@163.com>
2020-01-17 17:18:41 +0800

    获取设备信息

--- lib/device_mode.dart
+++ lib/device_mode.dart
@@ -4,7 +4,20 @@ import 'package:flutter/services.dart';
 
 class DeviceMode {
   static const MethodChannel _channel =
-      const MethodChannel('device_mode');
+      const MethodChannel('cn.waitwalker/device_mode');
+
+  ///
+  /// @name get device info
+  /// @description
+  /// @parameters
+  /// @return
+  /// @author waitwalker
+  /// @date 2020-01-17
+  ///
+  static Future<Map<String,dynamic>> get deviceModeInfo async{
+    final Map modeInfo = await _channel.invokeMethod("deviceInfo");
+    return modeInfo;
+  }
 
   static Future<String> get platformVersion async {
     final String version = await _channel.invokeMethod('getPlatformVersion');
